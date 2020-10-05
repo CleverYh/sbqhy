66614e77426be6c276b1071164da6fcea4e67263
CleverYh <cleveryh@qq.com>
2020-10-05 23:19:44 +0800

    intersection

--- intersection/setIntersection.h
+++ intersection/setIntersection.h
@@ -5,7 +5,8 @@ using namespace std;
 
 typedef vector<int> Set;
 
-Set setIntersection(const Set A, const Set B){
-    Set C;
-    return C;
-}
+Set setIntersection(const Set A, const Set B);
+// {
+//     Set C;
+//     return C;
+// }
